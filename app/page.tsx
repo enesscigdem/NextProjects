c5cbf1dc568f90be75bceba9608b411cbec6b341
Enes Çiğdem <enes.cigdem@bidy.com.tr>
2024-06-17 02:10:55 +0300

    Updat2e

--- app/page.tsx
+++ app/page.tsx
@@ -9,7 +9,7 @@ export default function Page() {
     return (
         <main className="flex min-h-screen flex-col p-6">
             <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
-                <AcmeLogo/>
+                <AcmeLogo />
             </div>
             <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
                 <div className="flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
@@ -30,8 +30,9 @@ export default function Page() {
                         href="/login"
                         className="flex items-center gap-5 self-start rounded-lg bg-blue-500 px-6 py-3 text-sm font-medium text-white transition-colors hover:bg-blue-400 md:text-base"
                     >
-                        <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6"/>
+                        <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6" />
                     </Link>
+                    <p className="text-red-600"> I&apos;m Blue!</p>
                 </div>
                 <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
                     <Image
@@ -46,7 +47,8 @@ export default function Page() {
                         width={560}
                         height={620}
                         className="block md:hidden"
-                        alt="Screenshot of the dashboard project showing mobile version"></Image>
+                        alt="Screenshot of the dashboard project showing mobile version"
+                    />
                 </div>
             </div>
         </main>
