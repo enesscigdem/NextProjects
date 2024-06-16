3f25d0e00e3d9f664802fed199a87dbf9e1b24bd
Enes Çiğdem <enes.cigdem@bidy.com.tr>
2024-06-17 02:07:37 +0300

    Update

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,6 @@ export default function Page() {
                     >
                         <span>Log in</span> <ArrowRightIcon className="w-5 md:w-6"/>
                     </Link>
-                    <h1 className="text-red-600"> I'm Blue!</h1>
                 </div>
                 <div className="flex items-center justify-center p-6 md:w-3/5 md:px-28 md:py-12">
                     <Image
